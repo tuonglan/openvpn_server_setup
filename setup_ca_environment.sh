92dc54ab46d619d2d2257e58aa9957eb38046747
Lan Do <lado@roku.com>
2024-09-05 23:55:04 +0000

    Script: openvpn - use openvpn docker to create keys

--- setup_ca_environment.sh
+++ setup_ca_environment.sh
@@ -18,6 +18,7 @@ export VPN_NAME=${VPN_NAME:-ovpn_server}
 export VPN_SUBNET_PREFIX=${VPN_SUBNET_PREFIX:-10.5.53}
 export VPN_ADDRESS=${VPN_ADDRESS:-lando.com}
 export VPN_PORT=${VPN_PORT:-11194}
+export VPN_SERVER_IMAGE=${VPN_SERVER_IMAGE}
 
 # Dependencies
 export VPN_TYPE=$2
@@ -25,6 +26,15 @@ export VPN_SUBNET=${VPN_SUBNET_PREFIX}.0
 export VPN_SUBNET_GATEWAY=${VPN_SUBNET_PREFIX}.1
 export VPN_IP_POOL_START=${VPN_SUBNET_PREFIX}.11
 export VPN_IP_POOL_END=${VPN_SUBNET_PREFIX}.255
+
+USER_ID=$(id -u $USER)
+GROUP_ID=$(id -g $USER)
+
+# Check if Openvpn image env variable is set
+if [ -z "$VPN_SERVER_IMAGE" ]; then
+    echo "VPN_SERVER_IMAGE is not set properly: $VPN_SERVER_IMAGE"
+    exit -1
+fi
 
 # Make the directoriesa
 echo "Making the directories & files"
@@ -41,7 +51,7 @@ easyrsa build-ca nopass
 echo "Generate the server certificate"
 easyrsa build-server-full $VPN_NAME nopass
 easyrsa gen-dh
-openvpn --genkey secret ${DIR}/server/ta.key
+sudo docker run --rm -it -u $USER_ID:$GROUP_ID -v ${DIR}:${DIR} ${VPN_SERVER_IMAGE} openvpn --genkey secret ${DIR}/server/ta.key
 easyrsa gen-crl
 
 # Finalize the data
